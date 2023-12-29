b063e437d2dad1f3a41b49844ea0952eb9720485
mhdoa <mohammadali4a@gmail.com>
2023-12-29 20:38:34 +0330

    Input Names
    Now can get tow name in first

---
+++ Main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    char Player[3][20];
+
+    scanf("%20s %20s", Player[1], Player[2]);
+    //printf("%s\n %s", Player[1], Player[2]);
+
+    return 0;
+}
