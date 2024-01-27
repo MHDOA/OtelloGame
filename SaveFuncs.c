a886d050d30ea5b5e142db285e0f98a78e8c43cb
mhdoa-ubuntu-home <mohammadali4a@gmail.com>
2024-01-27 19:26:06 +0330

    Add Initial Json, and Complete Set, Get, init, Read

--- SaveFuncs.c
+++ SaveFuncs.c
@@ -6,10 +6,10 @@ struct Player{
     int score;
 };
 
+void JsonInitial();
 cJSON *ReadJson();
 void SetScore(struct Player Players[3], int playerNum);
-void GetScore();
-void UpdateScore();
+void GetScore(struct Player Players[3], int playerNum);
 
 int main(){
 
@@ -19,20 +19,44 @@ int main(){
         {"karim", 24}
     };
     
-    SetScore(Players, 0);
+    //JsonInitial();
+    GetScore(Players, 1);
 
     return 0;
 }
 
-cJSON *ReadJson(){
+void JsonInitial(){
+    cJSON *json = cJSON_CreateObject();
+    cJSON *tables = cJSON_CreateObject();
+    cJSON *scores = cJSON_CreateObject();
+
+    cJSON_AddStringToObject(tables, "initial", "initial");
+    cJSON_AddNumberToObject(scores, "name", 0);
+    
+
+    cJSON_AddItemToObject(json, "Table", tables);
+    cJSON_AddItemToObject(json, "Score", scores);
 
     FILE *fp = fopen("data.json", "w");
-    /*if(fp == NULL){
-        FILE *fp = fopen("data.json", "w");
-    }*/
+    char *str = cJSON_Print(json);
+    fputs(str, fp);
+    cJSON_Delete(json);
+    fclose(fp);
+
+    printf("%s\n", str);
+}
+
+cJSON *ReadJson(){
+
+    FILE *fp = fopen("data.json", "r");
+    if(fp == NULL){
+        JsonInitial();
+        fp = fopen("data.json", "r");
+    }
 
     char buffer[1024];
     size_t len = fread(buffer, 1, 1024, fp);
+    fclose(fp);
 
     cJSON *json = cJSON_Parse(buffer);
 
@@ -41,26 +65,52 @@ cJSON *ReadJson(){
 
 void SetScore(struct Player Players[3], int playerNum){
     cJSON *json = ReadJson();
-    printf("r\n");
 
-    cJSON *ScoresJson = cJSON_GetObjectItem(json, "Score");
+    cJSON *ScoreJson = cJSON_GetObjectItem(json, "Score");
+    if(ScoreJson == NULL){
+        JsonInitial();
+        json = ReadJson();
+        ScoreJson = cJSON_GetObjectItem(json, "Score");
+    }
 
-    cJSON *playerScore = cJSON_GetObjectItem(ScoresJson, Players[playerNum].name);
+    cJSON *playerScore = cJSON_GetObjectItem(ScoreJson, Players[playerNum].name);
 
-    /*if(playerScore != NULL){
+    if(playerScore != NULL){
         int score = playerScore->valueint + Players[playerNum].score;
-        cJSON_ReplaceItemInObject(ScoresJson, Players[playerNum].name, cJSON_CreateNumber(score));
+        cJSON_SetNumberValue(playerScore, score);
     }
-    else {
-        cJSON_AddNumberToObject(ScoresJson, Players[playerNum].name, Players[playerNum].score);
+    else{
+        cJSON_AddNumberToObject(ScoreJson, Players[playerNum].name, Players[playerNum].score);
     }
 
-    cJSON_ReplaceItemInObject(json, "Score", ScoresJson);
-
-    FILE *fp = fopen("data.json", "w");*/
+    FILE *fp = fopen("data.json", "w");
     char *str = cJSON_Print(json);
-    //fputs(str, fp);
+    fputs(str, fp);
+    fclose(fp);
+    cJSON_Delete(json);
 
-    printf("%s", str);
+    //printf("%s", str);
 
+}
+
+void GetScore(struct Player Players[3], int playerNum){
+    cJSON *json = ReadJson();
+    cJSON *ScoreJson = cJSON_GetObjectItem(json, "Score");
+
+    if(ScoreJson == NULL){
+        JsonInitial();
+        json = ReadJson();
+        ScoreJson = cJSON_GetObjectItem(json, "Score");
+    }
+
+    //int size = cJSON_GetArraySize(ScoreJson);
+
+    cJSON *playerScore = cJSON_GetObjectItem(ScoreJson, Players[playerNum].name);
+    int score;
+    if(playerScore != NULL)
+        score = playerScore -> valueint;
+
+    cJSON_Delete(json);
+
+    //printf("%d", size);
 }
