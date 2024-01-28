694dbb5d543c230f52c9ca56a6f8f8f11ed52ba8
mhdoa-ubuntu-home <mohammadali4a@gmail.com>
2024-01-29 02:07:54 +0330

    Add crypt comment

--- Crypt.c
+++ Crypt.c
@@ -7,11 +7,15 @@ const int a = 127;
 const int a_inv = 127;
 
 void EncryptMessage(char* msg, char* cipher) {
-    for (int i = 0; i < strlen(msg); i++) {
 
-        if (!isspace(msg[i])&& msg[i] != '\0') {
-            cipher[i] = (char)((((a * (msg[i])) + b) % 128));
+    for (int i = 0; i < strlen(msg); i++) { // Go char to char
+
+        // Crypt char
+        if (!isspace(msg[i])&& msg[i] != '\0') { // Didn't convert white spaces or null
+            cipher[i] = (char)((((a * (msg[i])) + b) % 128)); // Crypting
         } 
+
+        // Add white spaces and null
         else {
             cipher[i] = msg[i];
         }
@@ -20,13 +24,15 @@ void EncryptMessage(char* msg, char* cipher) {
 
 void DecryptCipher(char* cipher, char* msg) {
     
-    for (int i = 0; i < strlen(cipher); i++) {
+    for (int i = 0; i < strlen(cipher); i++) { // Go char to char
 
-        if (!isspace(cipher[i]) && cipher[i] != '\0') {
-            msg[i] = (char)(((a_inv * ((cipher[i] - b))) % 128));
+        if (!isspace(cipher[i]) && cipher[i] != '\0') { // Didn't convert white spaces or null
+            msg[i] = (char)(((a_inv * ((cipher[i] - b))) % 128)); // Encrypting
         } 
+
+        // Add white spaces and null
         else {
-            msg[i] = cipher[i];
+            msg[i] = cipher[i]; 
         }
     }
 }
