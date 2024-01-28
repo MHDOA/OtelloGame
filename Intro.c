70f3fad3faaacc003252de2c37a0651187efd660
mhdoa-ubuntu-home <mohammadali4a@gmail.com>
2024-01-28 10:42:14 +0330

    Add Load game in intro

--- Intro.c
+++ Intro.c
@@ -19,15 +19,32 @@ int main(){
             system("gcc cJSON.c NormalMode.c -o normalMode.exe -lm");
             system("./normalMode.exe");
         }
-        else{
+        else if(n == 2){
             system("gcc cJSON.c TimingMode.c -o timingMode.exe -lm");
             system("./timingMode.exe");
+        }
+        else{
+            printf("Invalid Input. exit!\n");
         }
         
     }
     else if(n == 2){
-        system("gcc test.c -o test.exe");
-        system("./test.exe");
+        printf("1. Regular game\n2. Time game\n");
+        printf("Enter number: ");
+        
+        scanf("%d", &n);
+
+        if(n == 1){
+            system("gcc cJSON.c NormalModeLoad.c -o normalModeLoad.exe -lm");
+            system("./normalModeLoad.exe");
+        }
+        else if(n == 2){
+            system("gcc cJSON.c TimingModeLoad.c -o timingModeLoad.exe -lm");
+            system("./timingModeLoad.exe");
+        }
+        else{
+            printf("Invalid Input. exit!\n");
+        }
     }
     else{
         printf("Invalid Input. exit!\n");
